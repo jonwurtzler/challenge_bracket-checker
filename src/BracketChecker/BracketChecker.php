4eb6b6f6c7b99d171c0dad208a0ed267552dfa56
Jon Wurtzler <jon.wurtzler@gmail.com>
2015-11-04 18:11:46 -0600

    Initial change start

--- src/BracketChecker/BracketChecker.php
+++ src/BracketChecker/BracketChecker.php
@@ -39,95 +39,28 @@ class BracketChecker
     }
 
     /**
-     * Determine if the current bracket is escaped or not.
-     *
-     * @param string $bracketString
-     * @param int $pos
-     *
-     * @return bool
-     */
-    private function isEscaped($bracketString, $pos) {
-        if ($pos > 0) {
-            if ($bracketString[$pos-1] == "\\") { return true; }
-        }
-
-        return false;
-    }
-
-    /**
-     * Find a qualified closing bracket in the passed string.
-     *
-     * @param string $bracketString
-     * @param string $closingBracket
-     * @param int    $offset
-     *
-     * @return bool|int
-     */
-    private function findMatchingClosingBracket($bracketString, $closingBracket, $offset = -1)
-    {
-        // Search from the end of the string forward.
-        //   Passing a neg offset will force strrpos to start at end and work forward.
-        $matchingBracketPos = strrpos($bracketString, $closingBracket, $offset);
-
-        if ($matchingBracketPos) {
-            // Confirm the bracket wasn't escaped.
-            if (!$this->isEscaped($bracketString, $matchingBracketPos)) {
-                return $matchingBracketPos;
-
-                // Continue the search, ignoring found escaped character.
-                //   Use -2 to skip past the found '\' character as well.
-            } else {
-                $endingOffset = ((strlen($bracketString) - $matchingBracketPos) * -1) -1;
-                return $this->findMatchingClosingBracket($bracketString, $closingBracket, $endingOffset);
-            }
-        }
-
-        return null;
-    }
-
-    /**
      * Main Searching method.
-     *   Iterates through each character looking for an opening bracket or a closing one
+     *
      * @param $bracketString
      * @throws Exception
      */
     private function getBracketString($bracketString)
     {
         $length = strlen($bracketString);
+        $bracketStack = [];
 
         for ($i = 0; $i < $length; $i++) {
             // Skip any character following a '\'
             if ($bracketString[$i] == "\\") {
                 $i++;
+
             // Handle orphan closing bracket.
             } elseif (in_array($bracketString[$i], $this->closingBracketList)) {
                 throw new Exception("You have mismatched brackets.\nSpecifically a closing bracket without an opening bracket.\n");
+
             // Found an opening bracket
             } elseif (array_key_exists($bracketString[$i], $this->bracketList)) {
-                $closingBracket = $this->findMatchingClosingBracket($bracketString, $this->bracketList[$bracketString[$i]]);
-
-                // No closing bracket, stop process fully
-                if (empty($closingBracket)) {
-                    throw new Exception("You have mismatched brackets.\nSpecifically an opening bracket without a closing bracket.\n");
-                }
-
-                // Found a full pair, increment total
-                $this->bracketCount++;
-
-                // Look through substring for more brackets
-                $innerStrStart  = $i + 1;
-                $innerStrLength = $closingBracket - $innerStrStart;
-
-                // Update the current index to skip the found set
-                $i = $closingBracket;
-
-                // Any strings 2 or less are empty sets, skip looking for anything else.
-                if ($innerStrLength > 2) {
-                    $innerStr = substr($bracketString, $innerStrStart, $innerStrLength);
-
-                    // Run the sub string through same method to find brackets.
-                    $this->getBracketString($innerStr);
-                }
+                $bracketStack[] = $bracketString[$i];
             }
         }
     }
